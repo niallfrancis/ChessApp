aa4343281a3bf03787fabf7c6e2069f67d79a1b4
niallfrancis <niall.francis@students.plymouth.ac.uk>
2018-12-26 17:27:56 +0000

    Add parent classes and change initial board generation to use FEN

--- boardInfo.js
+++ boardInfo.js
@@ -103,6 +103,11 @@ function King(currentPos, colour) {
 
 //Set parent class for all pieces
 Pawn.prototype = new Piece();
+Knight.prototype = new Piece();
+Bishop.prototype = new Piece();
+Rook.prototype = new Piece();
+Queen.prototype = new Piece();
+King.prototype = new Piece();
 
 //Helper class for Bishop and Queen movement
 function DiagonalMovement(piece) {
@@ -164,57 +169,80 @@ function SpaceContainsEnemy(movingPiece, targetSpace) {
 
   return enemySpace;
 }
+
 function CreateNewBoardData(whitePieces, blackPieces)
 {
+  //rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR
+  boardData = GenerateFenBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+  return boardData;
+}
+
+function GenerateFenBoard(fen) {
+
   var boardSize = 120;
   var boardSpace = [];
-  var squareA8 = 22;
-  var squareH1 = 99;
+  var currentSquare = 21;
 
   for (var i = 0; i < boardSize; i++) {
     boardSpace[i] = 99;
   }
-  for (var i = 1; i <= boardSize; i++) {
-    if ((i >= 41 && i <= 48) ||
-        (i >= 51 && i <= 58) ||
-        (i >= 61 && i <= 68) ||
-        (i >= 71 && i <= 78)) {
-      //Empty starting spots
-      boardSpace[i] = 0;
+
+  for (var i = 0; i < fen.length; i++) {
+    var currentChar = fen.charAt(i);
+
+    if (currentChar != "/") {
+      if (!isNaN(currentChar)) {
+        for (var j = 0; j < parseInt(currentChar,10); j++) {
+          boardSpace[currentSquare] = 0;
+          currentSquare++;
+        }
+      } else {
+        switch (currentChar) {
+          case "p":
+            boardSpace[currentSquare] = new Pawn(currentSquare, "black");
+            break;
+          case "P":
+            boardSpace[currentSquare] = new Pawn(currentSquare, "white");
+            break;
+          case "n":
+            boardSpace[currentSquare] = new Knight(currentSquare, "black");
+            break;
+          case "N":
+            boardSpace[currentSquare] = new Knight(currentSquare, "white");
+            break;
+          case "b":
+            boardSpace[currentSquare] = new Bishop(currentSquare, "black");
+            break;
+          case "B":
+            boardSpace[currentSquare] = new Bishop(currentSquare, "white");
+            break;
+          case "r":
+            boardSpace[currentSquare] = new Rook(currentSquare, "black");
+            break;
+          case "R":
+            boardSpace[currentSquare] = new Rook(currentSquare, "white");
+            break;
+          case "q":
+            boardSpace[currentSquare] = new Queen(currentSquare, "black");
+            break;
+          case "Q":
+            boardSpace[currentSquare] = new Queen(currentSquare, "white");
+            break;
+          case "k":
+            boardSpace[currentSquare] = new King(currentSquare, "black");
+            break;
+          case "K":
+            boardSpace[currentSquare] = new King(currentSquare, "white");
+            break;
+          default:
+
+        }
+        currentSquare++;
+      }
     } else {
-      if ((i >= 31 && i <= 38) || (i >= 81 && i <= 88))
-      {
-        //Pawns
-        boardSpace[i] = '1';
-      } else if (i == 22 || i == 27 || i == 92 || i == 97) {
-        //Knights
-        boardSpace[i] = '2';
-      } else if (i == 23 || i == 26 || i == 93 || i == 96) {
-        //Bishops
-        boardSpace[i] = '3';
-      } else if (i == 21 || i == 28 || i == 91 || i == 98) {
-        //Rooks
-        boardSpace[i] = '4';
-      } else if (i == 94 || i == 25) {
-        //Queens
-        boardSpace[i] = '5';
-      } else if (i == 95 || i == 24) {
-        //Kings
-        boardSpace[i] = '6';
-      }
-
-      if ((i >= 21 && i <= 28) || (i >= 31 && i <= 38)) {
-        //Black starting spots
-        boardSpace[i] = 'B' + boardSpace[i];
-        blackPieces.push(boardSpace[i]);
-      } else if ((i >= 81 && i <= 88) || (i >= 91 && i <= 98)) {
-        //White starting spots
-        boardSpace[i] = 'W' + boardSpace[i];
-        whitePieces.push(boardSpace[i]);
-      }
-
-
+      currentSquare += 2;
     }
   }
+
   return boardSpace;
 }
