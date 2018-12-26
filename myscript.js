709319ca1d13ccd4200a27b2b0118639434784c4
niallfrancis <niall.francis@students.plymouth.ac.uk>
2018-12-26 18:19:51 +0000

    Add draw pieces method and piece images

--- myscript.js
+++ myscript.js
@@ -1,6 +1,23 @@
 $(document).ready(function () {
     InitBoard();
+    console.log(boardData);
+    DrawPieces(boardData);
 });
+
+ function DrawPieces(boardData) {
+   var squares = $(".square");
+   var currentSquare = 0;
+
+   for (var i = 0; i < boardData.length; i++) {
+     if (boardData[i] != 99) {
+       if (boardData[i] != 0) {
+         image = "<img class=\"piece " + boardData[i].colour + " " + boardData[i].pieceVal  + "\"/>";
+         $(".square").eq(currentSquare).append(image);
+       }
+       currentSquare++;
+     }
+   }
+ }
 
 function InitBoard() {
     var light = 1;
@@ -8,7 +25,7 @@ function InitBoard() {
     var rank;
     var file;
 
-    for (i = 8; i >= 1; i--) {
+    for (i = 1; i <= 8; i++) {
 
         light ^= 1;
         rank = "rank" + i;
